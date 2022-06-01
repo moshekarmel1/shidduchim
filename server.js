9316a187c2d670514eb17ad7fe8b0751d1473c4a
Karmel, Moshe <moshekarmel@quickenloans.com>
2022-06-01 16:19:01 -0400

    tweak

--- server.js
+++ server.js
@@ -362,5 +362,5 @@ app.get("/*", function (req, res) {
 const PORT = process.env.PORT || "4000";
 
 const server = app.listen("4000", function () {
-  console.log("Server is running!");
+  console.log(`Server is running on port ${PORT}!`);
 });
